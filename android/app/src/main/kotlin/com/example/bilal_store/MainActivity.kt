53b7631812398cf04024ea9240634755c86a8ea1
Muhammad Bilal <mbilalabid800@.com>
2024-07-28 13:58:45 +0500

    first

---
+++ android/app/src/main/kotlin/com/example/bilal_store/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bilal_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/app-constant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static String appMainName = "bilal store";
+  static String appPoweredBy = 'Powered By Techi4u';
+  static const appMainColor = Color(0xFFbf1b08);
+  static const appScendoryColor = Color(0xFF981206);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+}
